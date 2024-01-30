fec9387d45b21d8de8f2eb39a47ff23453be9be1
I-AM-EXCEPTIONAL <104685343+I-AM-EXCEPTIONAL@users.noreply.github.com>
2024-01-30 21:56:36 +0530

    Update urls.py

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 from . import views
+from .views import custom_csrf_failure_view
 
 
 urlpatterns = [
     path('', views.home, name='home')
+    path('csrf-failure/', custom_csrf_failure_view, name='csrf_failure'),
 ]
